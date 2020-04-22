2aff8aba3b7c70d12f00e942d810e4c2dca4d361
Kevin Stone <kevinastone@gmail.com>
2020-04-21 19:41:21 -0500

    Added rust-analyzer formula

---
+++ rust-analyzer.rb
@@ -0,0 +1,26 @@
+class RustAnalyzer < Formula
+  desc "Rust-analyzer is an implementation of Language Server Protocol for the Rust programming language"
+  homepage "https://rust-analyzer.github.io"
+  version "2020-04-20"
+  url "https://github.com/rust-analyzer/rust-analyzer/releases/download/#{version}/rust-analyzer-mac"
+  sha256 "e375d814123b019a5fd8b17ee37c55120778f320b46a88efcb8e8f9dff61909e"
+
+  # depends_on "rust" => :build
+
+  def install
+    bin.install "rust-analyzer-mac" => "rust-analyzer"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test rust-analyzer`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
